88468fb7e5242a9a876c53ba2e0e24bbe902e1cf
plumbz <plumby.eschenbacher@yahoo.com>
2024-10-19 23:03:05 +0200

    added 3rd movie

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,15 +8,27 @@ export const MainView = () => {
         {
             id: 1,
             title: 'Twilight',
+            description: 'vampire inlove with human',
             director: 'Catherine Hardwicke',
+            genre: 'Romantic',
             image: 'https://en.wikipedia.org/wiki/Twilight_(2008_film)#/media/File:Twilight_(2008_film)_poster.jpg'
         },
         {
             id: 2,
             title: 'Princess Diaries',
+            description: 'an ordenary teenager who later found out she was indeed a princess',
             director: 'Garry Marshall',
+            genre: 'comedy',
             image: 'https://en.wikipedia.org/wiki/The_Princess_Diaries_(film)#/media/File:Princess_diaries_ver1.jpg'
-        }
+        },
+        {
+            id: 3,
+            title: "The Matrix",
+            description: "A computer hacker learns the truth about reality and joins a rebellion against machines.",
+            director: 'Wachowskis',
+            genre: 'Actio',
+            image: "/images/thematrix.jpg",
+        },
     ]);
 
 
