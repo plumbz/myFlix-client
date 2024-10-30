be70ccb7a904778dcbbae74bd79c93843518983c
plumbz <plumby.eschenbacher@yahoo.com>
2024-10-30 15:32:07 +0100

    changed fetch from  local into heroku.

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -14,49 +14,48 @@ export const LoginView = ({ onLoggedIn }) => {
         };
 
         console.log(data);
-        fetch("http://localhost:8083/login", {
+        fetch("https://movie-flix19-efb939257bd3.herokuapp.com/login", {
             method: "POST",
             body: JSON.stringify(data),
             headers: {
                 "Content-Type": "application/json"
             }
         })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Login response: ", data);
+                if (data.user) {
+                    onLoggedIn(data.user, data.token);
+                } else {
+                    alert("No such user");
+                }
+            })
+            .catch((e) => {
+                alert("Something went wrong");
+            });
+    };
 
-    }).then((response) => response.json())
-        .then((data) => {
-            console.log("Login response: ", data);
-            if (data.user) {
-                onLoggedIn(data.user, data.token);
-            } else {
-                alert("No such user");
-            }
-        })
-        .catch((e) => {
-            alert("Something went wrong");
-        });
-};
-
-return (
-    <form onSubmit={handleSubmit}>
-        <label>
-            Username:
-            <input
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-            />
-        </label>
-        <label>
-            Password:
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-            />
-        </label>
-        <button type="submit">Submit</button>
-    </form>
-);
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+            </label>
+            <label>
+                Password:
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
