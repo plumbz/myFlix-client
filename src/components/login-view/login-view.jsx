7949ddcbdc8a625142e02730ab793ff61bce9f91
plumbz <plumby.eschenbacher@yahoo.com>
2024-10-30 14:31:15 +0100

    fixed issue from login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -14,55 +14,49 @@ export const LoginView = ({ onLoggedIn }) => {
         };
 
         console.log(data);
-        // fetch("https://movie-flix19-efb939257bd3.herokuapp.com/login", {
-        //     method: "POST",
-        //     body: JSON.stringify(data),
-        //     headers: {
-        //         "Content-Type": "application/json"
-        //     }
-        // })
-        const url = `https://movie-flix19-efb939257bd3.herokuapp.com/login?username=${username}&password=${password}`;
-
-        fetch(url, {
+        fetch("http://localhost:8083/login", {
             method: "POST",
-            // headers: {
-            //     "Content-Type": "application/json"
-            // }
-        }).then((response) => response.json())
-            .then((data) => {
-                console.log("Login response: ", data);
-                if (data.user) {
-                    onLoggedIn(data.user, data.token);
-                } else {
-                    alert("No such user");
-                }
-            })
-            .catch((e) => {
-                alert("Something went wrong");
-            });
-    };
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username:
-                <input
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                />
-            </label>
-            <label>
-                Password:
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
-    );
+    }).then((response) => response.json())
+        .then((data) => {
+            console.log("Login response: ", data);
+            if (data.user) {
+                onLoggedIn(data.user, data.token);
+            } else {
+                alert("No such user");
+            }
+        })
+        .catch((e) => {
+            alert("Something went wrong");
+        });
+};
+
+return (
+    <form onSubmit={handleSubmit}>
+        <label>
+            Username:
+            <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+            />
+        </label>
+        <label>
+            Password:
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+            />
+        </label>
+        <button type="submit">Submit</button>
+    </form>
+);
 };
