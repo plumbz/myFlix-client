0149ec2f666265f30ac41c74abe8fe9d4c7bfaa9
plumbz <plumby.eschenbacher@yahoo.com>
2024-11-01 23:01:46 +0100

    used col and row for movie-card , login and movie- view

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,17 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
 
 import "./index.scss";
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
-    return <MainView />;
+    return (
+
+        <Container style={{ border: "1px solid red" }}>
+            <MainView />
+        </Container>
+    );
 };
 
 // Finds the root of your app
